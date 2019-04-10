8d0d890e2048cedbfa9aaf240b248f12bbbfe237
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-04-09 23:53:37 -0300

    aula 101 promises pt 3

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {OfertasService} from '../ofertas.service'
+import {Oferta} from '../shared/oferta.model'
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
+})
+export class HomeComponent implements OnInit {
+
+public ofertas : Array<Oferta>
+
+  constructor(private ofertasService : OfertasService) { }
+
+  ngOnInit() {
+    //this.ofertas = this.ofertasService.getOfertas();
+    //console.log(this.ofertas);
+
+    this.ofertasService.getOfertas2()
+    .then(
+      (ofertas: Array<Oferta>) => { this.ofertas = ofertas},
+      (param : any) => {console.log(param)}
+      )
+  }
+
+}
