51c84c7f78dcf616bea4c14625f54c8a8eff1bf8
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-04-11 04:17:44 -0300

    aula 128 ofertas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import {Routes} from '@angular/router'
 import {HomeComponent} from '../app/home/home.component'
 import {RestaurantesComponent} from '../app/restaurantes/restaurantes.component'
 import {DiversaoComponent} from '../app/diversao/diversao.component'
+import {OfertaComponent} from '../app/oferta/oferta.component'
 
 export const ROUTES: Routes = [
     {path:'', component: HomeComponent},
     {path:'restaurantes', component: RestaurantesComponent},
-    {path:'diversao', component: DiversaoComponent}
+    {path:'diversao', component: DiversaoComponent},
+    {path:'oferta', component: OfertaComponent},
+    {path:'oferta/:id', component: OfertaComponent}
+    
 
 ]
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -3,23 +3,29 @@ import { reject } from 'q';
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import 'rxjs'
-
+import {URL_API} from './app.api'
 @Injectable()
 export class OfertasService{
 	constructor(private http : HttpClient){
 
 	}
-
+	public getOfertasPorId(id: number) : Promise <Oferta>{
+		return this.http.get(`${URL_API}?id=${id}`).toPromise()
+		.then((response : any)=> {
+return response.shift()
+		})
+	}
     public getOfertas() : Promise <Array<Oferta>>{
 		//requisicao
-		return this.http.get<Array<Oferta>>("http://localhost:3000/ofertas?destaque=true")
+		return this.http.get<Array<Oferta>>(`${URL_API}?destaque=true`)
 		.toPromise()
 		.then((resposta : any) => resposta)
 		
 		}
 		public getOfertasPorCategoria(categoria : string) : Promise <Array<Oferta>>{
-			return this.http.get<Array<Oferta>>(`http://localhost:3000/ofertas?categoria=${categoria}`)
+			return this.http.get<Array<Oferta>>(`${URL_API}?categoria=${categoria}`)
 			.toPromise()
 			.then((response : any) => response)
 		}
+		
 }
