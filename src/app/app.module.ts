44337886b08da6ca6134c9cacf711ee2581d56f0
Matheus Campelo <matheuscampelocavalcante@hotmail.com>
2019-04-16 14:40:37 -0300

    formularios do angular aula 198

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {RouterModule} from '@angular/router'
+import {FormsModule} from '@angular/forms'
 
 import {ROUTES} from './app.routes'
 
@@ -32,11 +33,13 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
     DescricaoReduzida,
     OrdemCompraComponent,
     OrdemCompraSucessoComponent
+    
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
